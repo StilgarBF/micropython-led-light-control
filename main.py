8764cec3592515fa9d76e65d448947e86b879a8d
Tino Dietel <tino.dietel@projekt2k.de>
2018-12-17 18:48:19 +0100

    adding comments and todo

--- main.py
+++ main.py
@@ -20,15 +20,22 @@ html = """ foooo"""
 
 
 def ana2pwm(analog_in):
-    pwmVal = 1030 / 4096 * analog_in
-    pwmVal = round(pwmVal / 10) * 10
+    """
+    calculate PWM value (0 - 1023) from analog in (0 - 4096)
+    Todo: change to actual brightness (log)
+    :param analog_in: integer
+    :return: integer
+    """
+    pwm_value = 1030 / 4096 * analog_in
+    pwm_value = round(pwm_value / 10) * 10
 
-    if pwmVal > 1023:
-        pwmVal = 1023
+    if pwm_value > 1023:
+        pwm_value = 1023
 
-    return round(pwmVal)
+    return round(pwm_value)
 
 
+# Todo: sockets seem to not die properly when restarting. check if socket.SO_REUSEADDR works
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.SO_REUSEADDR, 1)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 s.bind(('', 80))
@@ -36,6 +43,14 @@ s.listen(5)
 
 
 def web_process():
+    """
+    Intercept calls, set LED brightness accordingly.
+    Todo: create api to allow:
+        * full on/off of both strips
+        * percentage for both
+        * maybe change to mqtt
+    :return:
+    """
     conn, addr = s.accept()
     print("Got a connection from %s" % str(addr))
     request = conn.recv(1024)
@@ -59,9 +74,14 @@ def web_process():
     conn.close()
 
 
+# start a thread for the above
 _thread.start_new_thread(web_process, ())
 
 while True:
+    """
+    Read value from both potis, set brightness of both strips accordingly.
+    Todo: send to do nothing, when no real change happens. (prevent flicker)
+    """
     potVal = poti_1.read()
     pwmValue = ana2pwm(potVal)
     led_1.duty(pwmValue)
